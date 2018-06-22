4f2ea24fece5afccefbca22cfc80541866d24cf6
keerthanachinna <40225685+keerthanachinna@users.noreply.github.com>
2018-06-22 10:31:25 +0530

    Create indexhun.py

---
+++ indexhun.py
@@ -0,0 +1,11 @@
+def sorted(arr,start,end)
+if(start>end):
+return
+print sorted(arr,start*2+1,end)
+print(arr[start])
+print sorted(arr,start*2+2,end)
+def main():
+arr[]=[1,5,3,4,2,6]
+print(arr[i])
+print(arr,0,arr_size-1)
+return
